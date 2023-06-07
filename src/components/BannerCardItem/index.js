3b8cf37ccce635cac7ecb4c6db4fe0211e76b805
VineelaMuthyala <131747823+VineelaMuthyala@users.noreply.github.com>
2023-06-07 15:10:37 +0530

    Update index.js

--- src/components/BannerCardItem/index.js
+++ src/components/BannerCardItem/index.js
@@ -4,16 +4,17 @@ const Banner = props => {
   const {bannerCardsList} = props
   const {headerText, description, className} = bannerCardsList
   return (
-    <div className="card">
-      <div className={className}>
+    <li className={`${className} card`}>
+      <div>
         <h1 className="heading">{headerText}</h1>
         <p className="text">{description}</p>
         <button className="btn" type="button">
           Show More
         </button>
       </div>
-    </div>
+    </li>
   )
 }
 
 export default Banner
+
